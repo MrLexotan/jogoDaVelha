bba25b4db4c056c68b35825040b263d62e5bdeca
MrLexotan <leite.tavares.joao@gmail.com>
2021-08-20 16:57:02 -0300

    Adicionado possibilidades de jogadas

--- js/jogoDaVelha/js.js
+++ js/jogoDaVelha/js.js
@@ -1,10 +1,20 @@
 let jogador = 'x'
+let jogadas = 0
 
 function checkJogo(id) {
     const src = checkSrc(id)
 
     if(src === 'branco.png') {
-        document.getElementById(id).src = 'img/'+ jogador + '.png'
+        document.getElementById(id).src = 'img/'+ jogador + '.png';
+        jogadas++
+        if (checkVitoria()) {
+            alert(`=======  O jogador ${jogador} venceu  =======`)
+            location.reload()
+        }
+        if (jogadas >= 9) {
+            alert('======= Empate =======')
+            location.reload()
+        }
         if (jogador === 'x') {
             jogador = 'o'
         } else {
@@ -16,4 +26,31 @@ function checkJogo(id) {
 function checkSrc(id) {
     const src = document.getElementById(id).src
     return src.substring(src.length - 10, src.length)
+}
+
+function checkVitoria() {
+    if ((checkSrc("cel1")) === checkSrc('cel2') && checkSrc('cel1') === checkSrc('cel3') && checkSrc('cel1') !== 'branco.png') {
+        return true
+    }
+    if ((checkSrc("cel4")) === checkSrc('cel5') && checkSrc('cel4') === checkSrc('cel6') && checkSrc('cel4') !== 'branco.png') {
+        return true
+    }
+    if ((checkSrc("cel7")) === checkSrc('cel8') && checkSrc('cel7') === checkSrc('cel9') && checkSrc('cel7') !== 'branco.png') {
+        return true
+    }
+    if ((checkSrc("cel1")) === checkSrc('cel4') && checkSrc('cel1') === checkSrc('cel7') && checkSrc('cel1') !== 'branco.png') {
+        return true
+    }
+    if ((checkSrc("cel2")) === checkSrc('cel5') && checkSrc('cel2') === checkSrc('cel8') && checkSrc('cel2') !== 'branco.png') {
+        return true
+    }
+    if ((checkSrc("cel3")) === checkSrc('cel6') && checkSrc('cel3') === checkSrc('cel9') && checkSrc('cel3') !== 'branco.png') {
+        return true
+    }
+    if ((checkSrc("cel1")) === checkSrc('cel5') && checkSrc('cel1') === checkSrc('cel9') && checkSrc('cel1') !== 'branco.png') {
+        return true
+    }
+    if ((checkSrc("cel3")) === checkSrc('cel5') && checkSrc('cel3') === checkSrc('cel7') && checkSrc('cel3') !== 'branco.png') {
+        return true
+    }
 }
